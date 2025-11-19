a34c95c3fbb7aa625f64235d1327907ac24f8e92
felipecoimbra123 <felipe.2007coimbra@gmail.com>
2025-11-19 15:33:16 -0300

    Habilidades adicionadas ao index - com animação

--- script/index.js
+++ script/index.js
@@ -1,1 +1,16 @@
-qualquer coisa so para ir para o github
+function revealOnScroll() {
+    const elements = document.querySelectorAll(".reveal");
+
+    elements.forEach(el => {
+        const windowHeight = window.innerHeight;
+        const elementTop = el.getBoundingClientRect().top;
+        const visiblePoint = 120;
+
+        if (elementTop < windowHeight - visiblePoint) {
+            el.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
