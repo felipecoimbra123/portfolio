8829d55441bff2fe1e926a8ebf12208963cdb80e
felipecoimbra123 <felipe.2007coimbra@gmail.com>
2025-11-23 02:08:17 -0300

    Projeto finalizado 100%

--- script/index.js
+++ script/index.js
@@ -14,31 +14,3 @@ function revealOnScroll() {
 
 window.addEventListener("scroll", revealOnScroll);
 window.addEventListener("load", revealOnScroll);
-
-async function carregarProjetos() {
-    const usuario = "SEU_USUARIO_GITHUB";
-    const url = `https://api.github.com/users/${usuario}/repos`;
-
-    const resposta = await fetch(url);
-    const repos = await resposta.json();
-
-    const container = document.getElementById("repos");
-
-    repos.forEach(repo => {
-        const card = document.createElement("div");
-        card.classList.add("repo-card");
-
-        card.innerHTML = `
-            <h2>${repo.name}</h2>
-            <p>${repo.description || "Sem descrição disponível 😊"}</p>
-
-            <a href="${repo.html_url}" target="_blank">
-                Ver no GitHub →
-            </a>
-        `;
-
-        container.appendChild(card);
-    });
-}
-
-carregarProjetos();
